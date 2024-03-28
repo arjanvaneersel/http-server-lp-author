994cdac3b1e032799eec83ecf7335d82a5c772f7
Arjan van Eersel <arjanvaneersel@gmail.com>
2024-03-28 13:15:21 +0200

    Fulfill exercise

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
-use std::net::SocketAddr;
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, StatusCode};
+use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 /// This is our service handler. It receives a Request, routes on its
@@ -23,6 +23,15 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
             Ok(Response::new(Body::from(reversed_body)))
         }
 
+        (&Method::POST, "/parrot") => {
+            let body = hyper::body::to_bytes(req.into_body()).await?;
+            let reply = Body::from(format!(
+                "You said: {}",
+                String::from_utf8(body.to_vec()).unwrap()
+            ));
+            Ok(Response::new(reply))
+        }
+
         // Return the 404 Not Found for other routes.
         _ => {
             let mut not_found = Response::default();
@@ -42,7 +51,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let (stream, _) = listener.accept().await?;
 
         tokio::task::spawn(async move {
-            if let Err(err) = Http::new().serve_connection(stream, service_fn(handle_request)).await {
+            if let Err(err) = Http::new()
+                .serve_connection(stream, service_fn(handle_request))
+                .await
+            {
                 println!("Error serving connection: {:?}", err);
             }
         });
